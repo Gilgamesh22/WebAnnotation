d4dca03fa62d0b000833785556e8ab373549b251
GreenArrow <zaidnackasha22@gmail.com>
2017-12-15 02:56:56 -0500

    deletion now works.=)

--- public/app/modules/mainModule/js/controllers.js
+++ public/app/modules/mainModule/js/controllers.js
@@ -1,38 +1,71 @@
 validSelection = null;
 
 angular.module('App')
-.controller('NavItemController',['$scope', 'NavItems', function($scope, NavItems, Selection) {
+.controller('NavItemController',['$scope', 'NavItems', function($scope, NavItems) {
   $scope.navItems = NavItems;
-}])
-.controller('Annotate',['$scope', 'Selection', function($scope, Selection) {
-  $scope.color = "#aaf442";
-  $scope.annotationText = "";
-  $scope.Annotate = function() {
-    highlightRange = function (range, color) {
-      var newNode = document.createElement("div");
-      newNode.setAttribute(
-         "style",
-         "background-color: " + color + "; display: inline;"
-      );
-      try{
-        range.surroundContents(newNode);
-      } catch(error) {
-        alert(error);
-      };
-    };
 
-    if (Selection.selected) {
-      highlightRange(Selection.selected.getRangeAt(0), $scope.color);
+  $('#annotateBtn').on('click', function(){
+    $('#AnnotationDeleteBtn').css("visibility", "collapse");
+    $('#AnnotationUpdateBtn').css("visibility", "collapse");
+    $('#AnnotationAddBtn').css("visibility", "visible");
+    $('#addAnnotation').modal();
+  })
+}])
+.controller('Annotate',['$scope', 'Annotation', function($scope, Annotation) {
+  $scope.color = "#aaf442";
+  $scope.clearAnnotationText = true;
+
+  $("#addAnnotation").on('show.bs.modal', function () {
+    if ($scope.clearAnnotationText) {
+      $scope.annotationText = "";
     }
+  });
+
+  $scope.$watch(function () {
+    return $scope.annotationText;
+  },
+  function (newValue, oldValue) {
+      if(newValue == oldValue){return;}
+      $scope.annotationText = newValue;
+  }, true);
+
+  var AddAnnotation = function(){
+    Annotation.Add($scope.annotationText, $scope.color, function(uuid, data){
+      $('#AnnotationDeleteBtn').css("visibility", "visible");
+      $('#AnnotationUpdateBtn').css("visibility", "visible");
+      $('#AnnotationAddBtn').css("visibility", "collapse");
+
+      $scope.$apply(function() {
+        $scope.color = data.color;
+        $scope.annotationText = data.test;
+        $scope.uuid = uuid;
+        $scope.clearAnnotationText = false;
+      });
+      $('#addAnnotation').modal();
+      $scope.clearAnnotationText = true;
+    });
   };
+  $scope.Add = AddAnnotation;
+
+  var DeleteAnnotation = function(){
+    Annotation.Remove($scope.uuid);
+    $('#'+ $scope.uuid).replaceWith(document.createTextNode($('#'+ $scope.uuid).html()));
+  };
+
+  $scope.Delete = DeleteAnnotation;
+
+  $scope.Update = function(){
+    DeleteAnnotation();
+    AddAnnotation();
+  };
+
 }])
 .controller('IframeUpdate', ['$scope', 'Selection', function($scope, Selection){
 
   annotatable = $('#website');
   annotatable.on('mouseup mouseup', function() {
-    Selection.selected = window.getSelection();
+    Selection.setSelected(window.getSelection());
   });
-
 }])
 ;
 
--- public/app/modules/mainModule/js/factory.js
+++ public/app/modules/mainModule/js/factory.js
@@ -8,6 +8,79 @@ angular.module('App')
   ]
 })
 .factory('Selection', function(){
-  return {selected: null}
+  selected = null;
+  range = null
+  var SetSelected = function(selected){
+    this.selected = selected;
+    if (selected) {
+      this.range = selected.getRangeAt(0);
+    } else {
+      this.range = null;
+    }
+
+  }
+  var GetSelected = function() {
+    return this.selected;
+  }
+  var GetRange = function() {
+    return this.range;
+  }
+  return {
+    setSelected: SetSelected,
+    getSelected: GetSelected,
+    getRange: GetRange
+  };
 })
+.factory('GenerateID', function()
+{
+  uuidv4 = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  };
+  return {
+    GenUUID: uuidv4
+  };
+})
+.factory('Annotation', ['Selection', 'GenerateID', function(Selection, GenerateID){
+  selected = {};
+
+  var add = function(test, color, onClickCallback) {
+    highlightRange = function (uuid, range, color) {
+
+      var newNode = document.createElement("div");
+      newNode.setAttribute("style", "background-color: " + color + "; display: inline;");
+      newNode.setAttribute("id", uuid);
+
+      newNode.onclick = function(event) {
+        if (onClickCallback) {
+          onClickCallback(uuid, selected[this.id]);
+        }
+      };
+
+      try{
+        range.surroundContents(newNode);
+      } catch(error) {
+        alert(error);
+      };
+    };
+
+    if (Selection.getSelected()) {
+      uuid = GenerateID.GenUUID();
+      highlightRange(uuid, Selection.getRange(), color);
+      selected[uuid] = {test: test, color: color, range: Selection.getRange()};
+    }
+  };
+
+  var remove = function(uuid){
+    delete selected[uuid];
+  };
+
+
+  return {
+    Add: add,
+    Remove:  remove
+  };
+}])
 ;
